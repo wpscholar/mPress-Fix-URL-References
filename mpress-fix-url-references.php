4d61914e62c973859c563657a1f641c52499fb4a
Micah Wood <micah@mpress.co>
2012-11-17 03:08:07 -0500

    Initial commit

---
+++ mpress-fix-url-references.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Plugin Name: mPress Fix URL References
+ * Plugin URI:http://micahwood.me/wordpress-plugins/mpress-fix-url-references/
+ * Description: Easily fix URL references in your WordPress database.
+ * Author: Micah Wood
+ * Author URI: http://micahwood.me
+ * Version: 0.1
+ * License: GPL3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Copyright 2012 by Micah Wood - All rights reserved.
+ */
+
+define( 'MPRESS_FIX_URL_REFERENCES_VERSION', '0.1' );
+
+if ( ! class_exists( 'mPress_Fix_URL_References' ) ) {
+
+	class mPress_Fix_URL_References {
+
+		function __construct() {
+			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		}
+
+		/**
+		 * Add a menu page
+		 */
+		function admin_menu() {
+			$title = __( 'Fix URL References', 'mpress-fix-url-references' );
+			$slug = 'mpress-fix-url-references';
+			add_submenu_page( 'tools.php', $title, $title, 'manage_options', $slug, array( $this, 'page_content' ) );
+		}
+
+		/**
+		 * Menu page content - Allows users to enter a URL to be replaced in the database
+		 */
+		function page_content() { ?>
+			<div class="wrap">
+				<?php screen_icon( 'tools' ); ?>
+				<h2><?php _e( 'mPress Fix URL References', 'mpress-fix-url-references' ); ?></h2>
+				<div class="tool-box"><?php
+					if( ! empty( $_POST['replace_url'] ) && isset( $_POST['nonce'] ) && wp_verify_nonce( $_POST['nonce'], 'fix_url_references' ) ) {
+						self::update_database_url_references( preg_replace( '#/$#', '', trim( $_POST['replace_url'] ) ) );
+						echo '<p style="color:green">'. __( 'Database update is complete.', 'mpress-fix-url-references' ) .'</p>';
+						echo '<p><a href="'. esc_attr( admin_url( 'tools.php?page=mpress-fix-url-references' ) ) .'" class="button-primary">'. __( 'Fix More URLs', 'mpress-fix-url-references' ) .'</a></p>';
+					} else {
+						if( ! defined( 'WP_HOME' ) || ! defined( 'WP_SITEURL' ) ) {
+							echo '<p>'. __( 'You must set the WP_HOME and WP_SITEURL constants in your wp-config.php file to use this plugin.', 'mpress-fix-url-references' ) .'</p>';
+						} else {
+							if( isset( $_POST['replace_url'] ) )
+								echo '<p style="color:red;">'. __( 'Please enter a URL!', 'mpress-fix-url-references' ) .'</p>';
+							echo '<p>'. __( 'URL references in the database that match your input will be replaced with: ', 'mpress-fix-url-references' ) .'<code>'. home_url() .'</code></p>';
+							echo '<form method="post"><p>';
+							echo '<label for="replace_url">'. __( 'URL reference to be replaced:', 'mpress-fix-url-references' ) .'</label> ';
+							echo '<input type="url" id="replace_url" name="replace_url" size="60" placeholder="'. esc_attr( home_url() ) .'" /></p>';
+							wp_nonce_field( 'fix_url_references', 'nonce' );
+							echo '<p style="color:red;">'. __( '<strong>ALERT</strong>: Before running, please double check your entry and make sure you have backed up the database!', 'mpress-fix-url-references' ) . '</p>';
+							echo '<input type="submit" class="button-primary" value="'. esc_attr( __( 'Fix References', 'mpress-fix-url-references' ) ) .'" />';
+							echo '</form>';
+						}
+					} ?>
+				</div><!-- .tool-box -->
+			</div><!-- #wrap --><?php
+		}
+
+		/**
+		 * Replaces instances of a URL in the database with WP_HOME. Also updates the 'siteurl' and 'home'
+		 * WordPress options.
+		 *
+		 * @param string $old_url
+		 */
+		public static function update_database_url_references( $old_url ) {
+			if( defined( 'WP_SITEURL' ) )
+				update_option( 'siteurl', WP_SITEURL );
+			if( defined( 'WP_HOME' ) ) {
+				update_option( 'home', WP_HOME );
+				self::run_replacement_query( 'commentmeta', 'meta_value', $old_url, WP_HOME );
+				self::run_replacement_query( 'comments', 'comment_content', $old_url, WP_HOME );
+				self::run_replacement_query( 'links', 'link_url', $old_url, WP_HOME );
+				self::run_replacement_query( 'links', 'link_rss', $old_url, WP_HOME );
+				self::run_replacement_query( 'options', 'option_value', $old_url, WP_HOME );
+				self::run_replacement_query( 'posts', 'post_content', $old_url, WP_HOME );
+				self::run_replacement_query( 'posts', 'guid', $old_url, WP_HOME );
+				self::run_replacement_query( 'postmeta', 'meta_value', $old_url, WP_HOME );
+				self::run_replacement_query( 'usermeta', 'meta_value', $old_url, WP_HOME );
+			}
+		}
+
+		/**
+		 * Replace all instances of a string in a column of a specific WordPress table.
+		 *
+		 * @param string $table
+		 * @param string $field
+		 * @param string $old
+		 * @param string $new
+		 *
+		 * @return false|int
+		 */
+		public static function run_replacement_query( $table, $field, $old, $new ) {
+			/**
+			 * @var wpdb $wpdb
+			 */
+			global $wpdb;
+			$table = $wpdb->_escape( $table );
+			$field = $wpdb->_escape( $field );
+			$sql = $wpdb->prepare( "UPDATE {$wpdb->$table} SET {$field} = REPLACE( {$field}, %s, %s )", $old, $new );
+			return $wpdb->query( $sql );
+		}
+
+	}
+
+	new mPress_Fix_URL_References();
+
+}
---
+++ readme.txt
@@ -0,0 +1,89 @@
+=== mPress Fix URL References ===
+Contributors: woodent
+Donate link: http://micahwood.me/donate/
+Tags: mpress, database, migrate, migration, URL, link, links
+Requires at least: 3.2
+Tested up to: 3.4.2
+Stable tag: 0.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Easily fix URL references in your WordPress database.
+
+== Description ==
+
+The **mPress Fix URL References** plugin allows you to easily fix URL references in your WordPress database.
+
+= Why? =
+
+When you move your site from one domain to another, there is no built-in process for updating URL references in the database. For example, if you have internal links in your post content that point to the full URL of other pages on your site, these URL references will become invalid when you migrate your site to a new domain. If you aren't a developer who knows how to write MySQL queries, replacing potentially thousands of instances of incorrect URLs is a real pain.  No worries.  I'm a developer and I've done all the work for you.
+
+= How? =
+
+Using this plugin is simple:
+
+1. Ensure that you define the [WP_HOME](http://codex.wordpress.org/Editing_wp-config.php#Blog_address_.28URL.29) and [WP_SITEURL](http://codex.wordpress.org/Editing_wp-config.php#WordPress_address_.28URL.29) constants in your wp-config.php file.  It's not that hard.
+2. Install the plugin
+3. Activate the plugin
+4. In the WordPress admin, click on 'Fix URL References' in the 'Tools' menu
+4. Enter the URL that you want to have replaced
+5. Click on 'Fix References'
+
+= Features =
+
+* Clean, well written code that won't bog down your site
+
+== Installation ==
+
+= Prerequisites =
+If you don't meet the below requirements, I highly recommend you upgrade your WordPress install or move to a web host
+that supports a more recent version of PHP.
+
+* Requires WordPress version 3.2 or greater
+* Requires PHP version 5 or greater ( PHP version 5.2.4 is required to run WordPress version 3.2 )
+
+= The Easy Way =
+
+1. In your WordPress admin, go to 'Plugins' and then click on 'Add New'.
+2. In the search box, type in 'mPress Fix URL References' and hit enter.  This plugin should be the first and likely the only result.
+3. Click on the 'Install' link.
+4. Once installed, click the 'Activate this plugin' link.
+
+= The Hard Way =
+
+1. Download the .zip file containing the plugin.
+2. Upload the file into your `/wp-content/plugins/` directory and unzip
+3. Find the plugin in the WordPress admin on the 'Plugins' page and click 'Activate'
+
+= Usage Instructions =
+
+Before you do anything else, this plugin requires that you define the [WP_HOME](http://codex.wordpress.org/Editing_wp-config.php#Blog_address_.28URL.29) and [WP_SITEURL](http://codex.wordpress.org/Editing_wp-config.php#WordPress_address_.28URL.29) constants in your wp-config.php file.  
+
+Once the plugin is installed and activated, just click on 'Fix URL References' in the 'Tools' menu on the left hand side in the WordPress admin area.  Next, simply enter the old URL you want to have replaced and click 'Fix References'.  Yep... it's that easy.
+
+== Advanced Usage ==
+
+By default, this plugin only replaces URLs in specific tables and fields that would typically have URL references.  I understand that this doesn't meet everyone's use cases, so this plugin exposes a simple PHP function for replacing string content from any given table and field.
+
+Please only use this function if you know what you are doing:
+
+`mPress_Fix_URL_References::run_replacement_query( $table, $field, $old, $new );`
+
+This plugin replaces content in the 'post_content' field in the 'posts' table. The line of code below is from this plugin and is given as an example of how to use this function:
+
+    mPress_Fix_URL_References::run_replacement_query( 
+        'posts', 
+        'post_content', 
+        'http://www.old-domain.com', 
+        'http://www.new-domain.com' 
+    );
+
+Notice how the table name doesn't include the WordPress table prefix.  This gets added automatically.
+
+== Changelog ==
+
+= 0.1 =
+
+* Initial commit
+
+== Upgrade Notice ==
